6dc4ec48c4d0884ff3250e14836172ce836386e2
unknown <baering@baering-ws.ccp.ad.local>
2014-09-24 13:14:21 +0000

    Adding a credit that only shows when the site is referenced via iframe

---
+++ src/directives/frameCredit.js
@@ -0,0 +1,27 @@
+app.directive("frameCredit", [
+	function() {
+		return {
+			restrict: "E",
+			scope: {},
+			templateUrl: "templates/frame-credit.html",
+			link: function(scope, element, attrs) {
+				// http://stackoverflow.com/a/326076
+				function inIFrame() {
+					try {
+						return window.self !== window.top;
+					} catch (e) {
+						return true;
+					}
+				}
+
+				if(inIFrame()) {
+					scope.inFrame = true;
+				}
+				else {
+					scope.inFrame = false;
+				}
+				scope.inFrame = true;
+			}
+		};
+	}
+]);
